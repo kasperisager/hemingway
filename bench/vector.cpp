f6244f4b8dba2f0b56da2defde1c77d06a89c0ae
Kasper Isager <kasperisager@gmail.com>
2016-04-27 14:55:55 +0200

    Add all vector benchmarks

--- bench/vector.cpp
+++ bench/vector.cpp
@@ -7,6 +7,41 @@ using namespace lsh;
 
 std::vector<bool> c(128);
 
-BENCHMARK(vector, vector, 10000, 1000) {
+vector u = vector::random(128);
+vector v = vector::random(128);
+
+BENCHMARK(vector, vector, 10000, 200) {
   lsh::vector v(c);
+}
+
+BENCHMARK(vector, size, 10000, 75000) {
+  v.size();
+}
+
+BENCHMARK(vector, get, 10000, 18000) {
+  v.get(64);
+}
+
+BENCHMARK(vector, equals, 10000, 10000) {
+  bool f = v == u;
+}
+
+BENCHMARK(vector, dot_product, 10000, 10000) {
+  v * u;
+}
+
+BENCHMARK(vector, and, 10000, 500) {
+  v & u;
+}
+
+BENCHMARK(vector, hash, 10000, 28000) {
+  v.hash();
+}
+
+BENCHMARK(vector, distance, 10000, 9000) {
+  vector::distance(u, v);
+}
+
+BENCHMARK(vector, random, 10000, 10) {
+  vector::random(128);
 }
