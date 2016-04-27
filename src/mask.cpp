2f838363990e3ab04632e7c1f7967133ca5499a1
Kasper Isager <kasperisager@gmail.com>
2016-04-27 12:30:24 +0200

    Reorganize some inheritance

--- src/mask.cpp
+++ src/mask.cpp
@@ -3,14 +3,26 @@
 
 namespace lsh {
   /**
+   * Project a vector.
+   *
+   * @param vector The vector to project.
+   * @return The projected vector.
+   */
+  vector mask::project(const vector& v) const {
+    if (this->dimensions_ != v.size()) {
+      throw std::invalid_argument("Invalid vector size");
+    }
+
+    return *this->mask_ & v;
+  }
+
+  /**
    * Construct a new classic mask.
    *
    * @param dimensionality The dimensionality of vectors to mask.
    * @param width The number of dimensions in vector projections.
    */
   classic_mask::classic_mask(unsigned int d, unsigned int w) {
-    this->dimensions_ = d;
-
     std::random_device random;
     std::mt19937 generator(random());
     std::uniform_int_distribution<> indices (0, d - 1);
@@ -26,20 +38,6 @@ namespace lsh {
     std::unique_ptr<vector> p(new vector(c));
 
     this->mask_ = std::move(p);
-  }
-
-  /**
-   * Project a vector, reducing it to a dimensionality specified by this mask.
-   *
-   * @param vector The vector to project.
-   * @return The projected vector.
-   */
-  vector classic_mask::project(const vector& v) const {
-    if (this->dimensions_ != v.size()) {
-      throw std::invalid_argument("Invalid vector size");
-    }
-
-    return *this->mask_ & v;
   }
 
   /**
@@ -61,19 +59,5 @@ namespace lsh {
     std::unique_ptr<vector> p(new vector(c));
 
     this->mask_ = std::move(p);
-  }
-
-  /**
-   * Project a vector, reducing it to a dimensionality specified by this mask.
-   *
-   * @param vector The vector to project.
-   * @return The projected vector.
-   */
-  vector covering_mask::project(const vector& v) const {
-    if (this->dimensions_ != v.size()) {
-      throw std::invalid_argument("Invalid vector size");
-    }
-
-    return *this->mask_ & v;
   }
 }
--- src/mask.hpp
+++ src/mask.hpp
@@ -9,18 +9,7 @@
 
 namespace lsh {
   class mask {
-    public:
-      /**
-       * Project a vector.
-       *
-       * @param vector The vector to project.
-       * @return The projected vector.
-       */
-      virtual vector project(const vector& vector) const = 0;
-  };
-
-  class classic_mask: public mask {
-    private:
+    protected:
       /**
        * The dimensionality of vectors to mask.
        */
@@ -33,20 +22,23 @@ namespace lsh {
 
     public:
       /**
+       * Project a vector.
+       *
+       * @param vector The vector to project.
+       * @return The projected vector.
+       */
+      vector project(const vector& vector) const;
+  };
+
+  class classic_mask: public mask {
+    public:
+      /**
        * Construct a new classic mask.
        *
        * @param dimensionality The dimensionality of vectors to mask.
        * @param width The number of dimensions in vector projections.
        */
       classic_mask(unsigned int dimensions, unsigned int width);
-
-      /**
-       * Project a vector.
-       *
-       * @param vector The vector to project.
-       * @return The projected vector.
-       */
-      vector project(const vector& vector) const override;
   };
 
   class covering_mask: public mask {
@@ -56,18 +48,6 @@ namespace lsh {
        */
       typedef std::vector<vector> mapping;
 
-    private:
-      /**
-       * The dimensionality of vectors to mask.
-       */
-      unsigned int dimensions_;
-
-      /**
-       * The vector mask to use.
-       */
-      std::unique_ptr<vector> mask_;
-
-    public:
       /**
        * Construct a new covering mask.
        *
@@ -76,13 +56,5 @@ namespace lsh {
        * @param mapping The random vector mapping to use for the mask.
        */
       covering_mask(unsigned int dimensions, unsigned int vector, const mapping& mapping);
-
-      /**
-       * Project a vector.
-       *
-       * @param vector The vector to project.
-       * @return The projected vector.
-       */
-      vector project(const vector& vector) const override;
   };
 }
