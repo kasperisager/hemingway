e938f22445cacadcf15e0d84e8aede8d4c421619
Kasper Isager <kasperisager@gmail.com>
2016-04-28 21:00:41 +0200

    Randomize table benchmark

--- bench/table.cpp
+++ bench/table.cpp
@@ -9,8 +9,6 @@ using namespace lsh;
 table t_cla({.dimensions = 128, .width = 32, .partitions = 64});
 table t_cov({.dimensions = 128, .radius = 5});
 
-vector v = vector::random(128);
-
 BENCHMARK(table, table_classic, 500, 4) {
   table({.dimensions = 128, .width = 32, .partitions = 64});
 }
@@ -19,18 +17,18 @@ BENCHMARK(table, table_covering, 500, 2) {
   table({.dimensions = 128, .radius = 5});
 }
 
-BENCHMARK(table, insert_classic, 10000, 10) {
-  t_cla.insert(v);
+BENCHMARK(table, insert_classic, 1500, 10) {
+  t_cla.insert(vector::random(128));
 }
 
-BENCHMARK(table, insert_covering, 10000, 10) {
-  t_cov.insert(v);
+BENCHMARK(table, insert_covering, 1500, 10) {
+  t_cov.insert(vector::random(128));
 }
 
-BENCHMARK(table, query_classic, 10000, 10) {
-  t_cla.query(v);
+BENCHMARK(table, query_classic, 1500, 10) {
+  t_cla.query(vector::random(128));
 }
 
-BENCHMARK(table, query_covering, 10000, 10) {
-  t_cov.query(v);
+BENCHMARK(table, query_covering, 1500, 10) {
+  t_cov.query(vector::random(128));
 }
