75e9543ac5484a7432d79b5946652e70f2ee2c24
Kasper Isager <kasperisager@gmail.com>
2016-05-08 13:30:00 +0200

    Adjust table benchmark counts

--- bench/table.cpp
+++ bench/table.cpp
@@ -9,26 +9,26 @@ using namespace lsh;
 table t_cla({.dimensions = 128, .samples = 32, .partitions = 64});
 table t_cov({.dimensions = 128, .radius = 5});
 
-BENCHMARK(table, table_classic, 500, 4) {
+BENCHMARK(table, table_classic, 500, 12) {
   table({.dimensions = 128, .samples = 32, .partitions = 64});
 }
 
-BENCHMARK(table, table_covering, 500, 2) {
+BENCHMARK(table, table_covering, 500, 4) {
   table({.dimensions = 128, .radius = 5});
 }
 
-BENCHMARK(table, insert_classic, 1500, 10) {
+BENCHMARK(table, insert_classic, 1500, 14) {
   t_cla.insert(vector::random(128));
 }
 
-BENCHMARK(table, insert_covering, 1500, 10) {
+BENCHMARK(table, insert_covering, 1500, 14) {
   t_cov.insert(vector::random(128));
 }
 
-BENCHMARK(table, query_classic, 1500, 10) {
+BENCHMARK(table, query_classic, 1500, 14) {
   t_cla.query(vector::random(128));
 }
 
-BENCHMARK(table, query_covering, 1500, 10) {
+BENCHMARK(table, query_covering, 1500, 14) {
   t_cov.query(vector::random(128));
 }
